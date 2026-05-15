e956844cea2e77948014810e5d690f99151220fe
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2026-05-15 09:48:52 +0200

    add dictionaries

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,2 +1,9 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
+person={
+    "first_name": "Noella",
+    "last_name": "Promise",
+    "age": 27
+}
+print(person.values())
