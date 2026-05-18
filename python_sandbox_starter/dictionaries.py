b2a81ff85ceca39f70ffaeb0e78f2c2d042298fd
Noella claire d'Assise Dushakimana <dushakenoella@gmail.com>
2026-05-18 14:21:41 +0200

    add logic

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,9 +1,16 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
 
-person={
-    "first_name": "Noella",
-    "last_name": "Promise",
-    "age": 27
-}
-print(person.values())
+# person={
+#     "first_name": "Noella",
+#     "last_name": "Promise",
+#     "age": 27
+# }
+
+
+# print(person.values())
+
+for x in range(1,10):
+    if x % 2 == 0:
+        print(x)
+print("we have 4 even numbers in the range")
